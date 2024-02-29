c74cf379d4d71e5f56af020697a498f8512dab1a
Kacey Na <kacey.na01@gmail.com>
2024-02-29 17:17:13 -0600

    NPC Checkpoint Animation added

--- Assets/Scripts/RespawnPoint.cs
+++ Assets/Scripts/RespawnPoint.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class RespawnPoint : MonoBehaviour
 {
     [field: SerializeField] public int checkpointNum { get; private set; }
+
+    public Animator NPC_Animator;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -12,6 +15,7 @@ public class RespawnPoint : MonoBehaviour
             Debug.Log("Set checkpoint " + checkpointNum + ".");
             // set game managers respawn coords to set coordinates
             LevelData.setCheckpoint(checkpointNum);
+            NPC_Animator.Play("Base Layer.NPC_B_OD_Checkpoint");
         }
     }
 }
