bf4cd3f58442cb51551c826282bcd8cb20a5ad93
Kacey Na <kacey.na01@gmail.com>
2024-03-19 17:31:33 -0500

    Button animation when hovered

--- Assets/Scripts/MouseOverUI.cs
+++ Assets/Scripts/MouseOverUI.cs
@@ -1,15 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using DG.Tweening;
 
 public class MouseOverUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public bool mouseOverPanel;
+    public bool isAnimated;
+    public float animDuration = 1.0f;
+    public float animStrength = 5.0f;
+
+    public void ResetTween()
+    {
+        transform.DORewind();
+    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         mouseOverPanel = true;
+        if (isAnimated)
+        {
+            ResetTween();
+            // transform.DOShakePosition(animDuration, animStrength, randomness:0f).onComplete = ResetTween;
+            // transform.DOShakeRotation(animDuration, animStrength).onComplete = ResetTween; ;
+            // transform.DOJump(transform.position, animStrength, 1, animDuration).SetEase(Ease.OutExpo).onComplete = ResetTween;
+            transform.DOJump(transform.position, animStrength, 1, animDuration).onComplete = ResetTween;
+        }
+        
     }
 
     public void OnPointerExit(PointerEventData eventData)
