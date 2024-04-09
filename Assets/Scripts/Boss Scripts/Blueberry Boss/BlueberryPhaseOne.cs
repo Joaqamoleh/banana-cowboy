4e1d26614ee652aa019512a6848b4090ba027fd4
Jonathan Kump <landon2002@gmail.com>
2024-04-09 17:29:53 -0500

    Cannon phase two, red indication for attack

--- Assets/Scripts/Boss Scripts/Blueberry Boss/BlueberryPhaseOne.cs
+++ Assets/Scripts/Boss Scripts/Blueberry Boss/BlueberryPhaseOne.cs
@@ -40,10 +40,16 @@ public class BlueberryPhaseOne : BossController
     GameObject[] disableOnEnd;
     [SerializeField]
     GameObject[] enableOnEnd;
+    [SerializeField]
+    BreakableCannon cannonPhaseOne;
 
     [Header("Phase 2")]
     [SerializeField]
     GameObject[] enableOnBegin;
+    [SerializeField]
+    BreakableCannon cannonPhaseTwo;
+
+
 
     [SerializeField]
     GameObject healthUIHolder;
@@ -256,13 +262,21 @@ public class BlueberryPhaseOne : BossController
         if (debugAttack != null)
         {
             RepositionSwordAttackTrigger();
-            debugAttack.SetActive(state == State.ATTACK || state == State.WINDUP || state == State.STUCK);
+            debugAttack.SetActive(state == State.ATTACK || state == State.WINDUP);
         }
     }
 
     void OnEndFightCutscene(CutsceneObject obj)
     {
         GetComponent<CannonController>().ShouldFireBombs(true);
+        if (cannonPhaseOne != null && !cannonPhaseOne.IsBroken())
+        {
+            cannonPhaseOne.BreakCannon();
+        }
+        if (cannonPhaseTwo != null)
+        {
+            cannonPhaseTwo.gameObject.SetActive(true);
+        }
     }
 
     void RepositionSwordAttackTrigger()
